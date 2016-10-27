d0f169ff691b345b5d8e9a51974ae8c13d8e9b79
Serg Kondrashov <kondr.s.a@yandex.ru>
2016-10-27 10:50:43 +1000

    Add new func: GetIUBInterface

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+__author__ = 'sergey.kondrashov'
+
 from time import strftime
 from getpass import getpass
 
@@ -7,19 +9,22 @@ def main():
     i = int(input("""Menu:
     1. Get serial numbers from juniper
     2. Get software version from Juniper
-    3. -                                    # Todo: compare configuration on NodeB/eNodeB and router
-    >>> """))
+    3. Get interface for IuB
+    4. -
+    >>> """))                                                   #Todo: compare configuration on NodeB/eNodeB and router
     if i == 1:
         command = GetSerialNumbers
     elif i == 2:
         command = GetSoftwareVersion
+    elif i == 3:
+        command = GetIUBInterface
     else:
         exit('[-] wrong choice')
     login = input('username: ').encode('utf-8') + '\r'.encode('utf-8')
     password = input('password: ').encode('utf-8') + '\r'.encode('utf-8')
     source_file = r'to_check.csv'
     result_file = source_file[:-3] + strftime('_%Y-%m-%d_%H-%M') + '.result.csv'
-    
+
     work = MultithreadWorking(command, login, password, source_file, result_file)
     work.start()
 
